afa868849725bd7405fc9735f839e7cc843ad524
Nemanja Radojkovic <radojkovic.n@gmail.com>
2024-01-21 23:51:25 +0100

    fixed run.sh

--- run.sh
+++ run.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
-gunicorn api:app \
-    --chdir ./src/fast_guni_app
+gunicorn fast_guni_app.api:app \
     --workers=1 \
     --bind=0.0.0.0 \
-    --worker-class=uvicorn.workers.UvicornWorker \
-    --logger-class=fast_guni_app.logging.UnifiedLogger
+    --worker-class uvicorn.workers.UvicornWorker \
+    --logger-class fast_guni_app.logging.UnifiedLogger
